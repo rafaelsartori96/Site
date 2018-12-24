107073f5ce502a7520d60daae9f5f47cccb54f14
rafaelsartori96 <rafael.sartori96@gmail.com>
2018-12-24 07:24:48 -0200

    Criando comando para exportar atas ao novo site

--- institucional/admin.py
+++ institucional/admin.py
@@ -8,3 +8,26 @@ class AdminAta(admin.ModelAdmin):
     search_fields = ('title', 'time')
 
 admin.site.register(Ata ,AdminAta)
+
+
+# Criamos um arquivo JSON com todas as informações das atas para o novo site
+import json
+
+# conteudo, data_criacao
+# highlights = None
+
+json_lista = []
+atas = Ata.objects.all()
+
+for ata in atas:
+    json_ = {
+        'conteudo': str(ata.content),
+        'data_criacao': str(ata.time),
+        'highlights': ata.title,
+    }
+
+    json_lista.append(json_)
+
+# Agora imprimimos
+with open('atas.json', 'w') as arquivo:
+    json.dump(json_lista, arquivo)
